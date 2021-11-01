4ae1ff72116ea8f7a00e6ae7df10e96be7f454a3
isjaoe <2020521h@stu.kobe-u.ac.jp>
2021-11-01 12:39:33 +0900

    section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -11,6 +11,29 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+    // (0+1)÷100の余り=1, (1++1)÷100の余り=2...って増えてって、(99+1)÷100の余り=0,(100+1)÷100の余り=1...って戻ってく
+
+  if(keyIsDown(" ".charCodeAt(0))){
+    if(count<25){
+      ellipse(width/2,height/2, 2*count+50)
+    }
+    else if(count<50){
+      ellipse(width/2,height/2, -2*count+150)
+    }
+    else if(count<75){
+      ellipse(width/2,height/2, 2*count-50)
+    }
+    else{
+      ellipse(width/2,height/2, -2*count+250)
+    }
+  }
+
+   else{
+     if(count<50){
+        ellipse(width/2,height/2,count+50)
+     }
+     else{
+          ellipse(width/2,height/2,150-count)
+     }
+   }
 }
