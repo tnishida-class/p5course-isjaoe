0a9ae148c9dc4bf3f0bd52dafe09196b10fcb50d
isjaoe <2020521h@stu.kobe-u.ac.jp>
2021-11-15 14:15:37 +0900

    section5-1,2,q1

--- section5-1/sketch.js
+++ section5-1/sketch.js
@@ -2,13 +2,26 @@
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background(0,0,255);
+  fill(255,241,0)
+  noStroke()
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    star(x, y, 10);
   }
+}
+
+function star(cx, cy, r){
+  beginShape();            // 点つなぎを始める
+  for(var i = 0; i < 5; i++){
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);           // 次につなぐ点を１つ増やす
+  }
+  endShape(CLOSE);         // 点つなぎを終わる
 }
 
 // ヒント：section5-2 にある star 関数をここにコピーして、 draw 内で ellipse の代わりに使おう
--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,20 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("神戸大学",100,128,0,128,255,255,0);
 }
 
-function balloon(t){
+function balloon(t,p,r1,g1,b1,r2,g2,b2){
+//t=テキスト、p=テキストの場所、r1.g1.b1=背景色、r2.g2.b2=文字色
   let w = textWidth(t);
-  let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  let h = textAscent() + textDescent(); //テキストの高さ。ベースラインから上の長さはAscent、ベースラインから下の長さはDescent
+  fill(r1,g1,b1);
+  noStroke()
+  // rect(0, 0, w + p * 10, h + p * 2); //(左上のx,左上のy,幅,高さ)
+  ellipse(p+w/2, h + p, w*3 ,80)//ellipse(中心x,中心y,横の直径,縦の直径)
+  fill(r2,g2,b2);
+  text(t, p, h + p); //(text,左上のx,左上のy)
+  fill(r1,g1,b1)
+  noStroke()
+  triangle(p+w/2+20,h+p+30,p+w/2+40,h+p+60,p+w/2+40,h+p+30)
 }
